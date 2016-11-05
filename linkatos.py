14b6c0ff5b8d07fd8ef5e358f3a7e443479a6d24
iwi <iwi@tuta.io>
2016-11-05 13:18:10 +0000

    feat: add bot name

--- linkatos.py
+++ linkatos.py
@@ -38,6 +38,10 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
+                slack_client.api_call("chat.postMessage", \
+                    channel = output['channel'], \
+                    text = "r u talkin' to me?", as_user = True)
+
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
                        output['channel']
@@ -47,7 +51,7 @@ def parse_slack_output(slack_rtm_output):
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
+        print("linkatos is connected and running!")
         while True:
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
