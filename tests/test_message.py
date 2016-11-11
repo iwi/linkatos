4168b5fe6aec451fa53d1770d19cec5126ee6560
iwi <iwi@tuta.io>
2016-11-11 23:03:43 +0000

    delete: example test

--- tests/test_message.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import linkatos.message as message
-
-def test_contains_a_https_url():
-    expected = "https://example.org"
-
-    assert message.contains_a_link("foo https://example.org") == expected
