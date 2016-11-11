beb4391b899849fefc5e3e3988a2545511274564
iwi <iwi@tuta.io>
2016-11-11 18:09:39 +0000

    test: add link detection test

--- tests/test_message_contains_a_link.py
+++ tests/test_message_contains_a_link.py
@@ -5,3 +5,15 @@ def test_contains_a_https_url():
     expected = "https://example.org"
 
     assert message_contains_a_link("foo https://example.org") == expected
+
+
+def test_contains_a_http_url():
+    expected = "http://example.com"
+
+    assert message_contains_a_link("foo http://example.com bar") == expected
+
+def test_is_not_a_url():
+
+    assert message_contains_a_link("foo htts://example.org") == None
+
+
