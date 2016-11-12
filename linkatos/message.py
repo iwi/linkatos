b87de9f2136257c0bc5a5e8842881c69fc68ea2f
iwi <iwi@tuta.io>
2016-11-12 14:40:59 +0000

    feat: group functions related to messages

--- linkatos/message.py
+++ linkatos/message.py
@@ -1,3 +1,5 @@
+#! /bin/env python
+
 import re
 
 link_re = re.compile("https?://\S+(\s|$)")
@@ -9,6 +11,19 @@ def contains_a_link(message):
 
     answer = link_re.search(message)
 
+    if answer is not None:
+        answer = answer.group(0).strip()
+
+    return answer
+
+
+yes_re = re.compile("(Yes|YES|yes)")
+
+def contains_a_yes(message):
+    """
+    Returns a yes if it matches the regex
+    """
+    answer = yes_re.search(message)
     if answer is not None:
         answer = answer.group(0).strip()
 
