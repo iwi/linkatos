c0940057e514edb329589ee9aca64672792332b9
iwi <iwi@tuta.io>
2016-11-12 17:09:11 +0000

    fix: expand yes regex

--- linkatos/message.py
+++ linkatos/message.py
@@ -1,7 +1,7 @@
 import re
 
 link_re = re.compile("https?://\S+(\s|$)")
-yes_re = re.compile("(Yes|YES|yes)")
+yes_re = re.compile("(/b|^)(Yes|YES|yes)(/b|,|/.|$)")
 
 def extract_url(message):
     """
