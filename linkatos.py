763630bc0aa3963cd76adf8ad88ec2a8b2d4c005
iwi <iwi@tuta.io>
2016-11-22 23:20:13 +0000

    refactor: Change big part of main to a function

--- linkatos.py
+++ linkatos.py
@@ -14,6 +14,41 @@ SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 # instantiate Slack clients
 slack_client = SlackClient(SLACK_BOT_TOKEN)
 
+def keep_wanted_urls(expecting_confirmation, url):
+    # parse the messages. Get a dictionary with @out, @channel,
+    # @out_type
+    parsed_message = parser.parse(slack_client.rtm_read(), BOT_ID)
+
+    print(parsed_message)
+    print(expecting_confirmation)
+
+    if utils.is_fresh_url(expecting_confirmation, parsed_message['type']):
+        url = parsed_message['out']
+
+    print(url)
+
+    printer.ask_confirmation(expecting_confirmation, parsed_message,
+                             slack_client)
+
+    # update expecting_confirmation
+    # when it's a url
+    expecting_confirmation = confirmation.update_confirmation_if_url(
+        parsed_message,
+        expecting_confirmation)
+
+    # Check if there is an answer
+    (expecting_confirmation, is_yes) = confirmation.process_confirmation_if_yn(
+                                            parsed_message,
+                                            expecting_confirmation)
+
+    # printer.notify_confirmation(expecting_confirmation, is_yes)
+
+    # Store url
+    # store_url(expecting_confirmation, url, is_yes)
+
+    time.sleep(READ_WEBSOCKET_DELAY)
+
+    return (expecting_confirmation, url)
 
 # Main
 if __name__ == '__main__':
@@ -26,38 +61,7 @@ if __name__ == '__main__':
         url = None
 
         while True:
-            # parse the messages. Get a dictionary with @out, @channel,
-            # @out_type
-            parsed_message = parser.parse(slack_client.rtm_read(), BOT_ID)
-
-            print(parsed_message)
-            print(expecting_confirmation)
-
-            if utils.is_fresh_url(expecting_confirmation, parsed_message['type']):
-                url = parsed_message['out']
-
-            print(url)
-
-            printer.ask_confirmation(expecting_confirmation, parsed_message,
-                                     slack_client)
-
-            # update expecting_confirmation
-            # when it's a url
-            expecting_confirmation = confirmation.update_confirmation_if_url(
-                parsed_message,
-                expecting_confirmation)
-
-            # Check if there is an answer
-            (expecting_confirmation, is_yes) = confirmation.process_confirmation_if_yn(
-                                                    parsed_message,
-                                                    expecting_confirmation)
-
-            # printer.notify_confirmation(expecting_confirmation, is_yes)
-
-            # Store url
-            # store_url(expecting_confirmation, url, is_yes)
-
-            time.sleep(READ_WEBSOCKET_DELAY)
+            (expecting_confirmation, url) = keep_wanted_urls(expecting_confirmation, url)
 
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
