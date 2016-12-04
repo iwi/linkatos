96799234e3774c9860c86e9e960f61b1a781aa2d
iwi <iwi@tuta.io>
2016-12-04 21:33:17 +0000

    feat: Add function to store urls to firebase

---
+++ linkatos/firebase.py
@@ -0,0 +1,33 @@
+import pyrebase
+
+
+def store_url(is_yes, url, FB_API_KEY, FB_USER, FB_PASS):
+    # connect to firebase
+    config = {
+        "apiKey": FB_API_KEY,
+        "authDomain": "coses-acbe6.firebaseapp.com",
+        "databaseURL": "https://coses-acbe6.firebaseio.com",
+        "storageBucket": "coses-acbe6.appspot.com"}
+
+    # do nothing if it's unnecessary
+    if is_yes is False:
+        return is_yes
+
+    # otherwise keep url
+    firebase = pyrebase.initialize_app(config)
+
+    # creates token every time maybe worth doing it once every 30m as they
+    # expire every hour
+    auth = firebase.auth()
+    user = auth.sign_in_with_email_and_password(FB_USER, FB_PASS)
+
+    db = firebase.database()
+    data = {
+        "url": url
+    }
+
+    db.child("users").push(data, user['idToken'])
+
+    is_yes = False
+
+    return is_yes
