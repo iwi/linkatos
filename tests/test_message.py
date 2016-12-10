197010bb83b4da3f89b7aa69e84ab067ee863e7b
iwi <iwi@tuta.io>
2016-12-10 16:35:36 +0000

    test: Add specific url characters test

--- tests/test_message.py
+++ tests/test_message.py
@@ -4,9 +4,43 @@ import linkatos.message as message
 
 # test url detection
 
-def test_url_detection_of_https():
+def test_basic_url_detection_of_https():
     expected = "https://example.org"
     assert message.extract_url("foo <https://example.org>") == expected
+
+def test_hash_in_url():
+    expected = "https://#.org"
+    assert message.extract_url("<https://#.org>") == expected
+
+
+def test_plus_in_url():
+    expected = "https://+.org"
+    assert message.extract_url("<https://+.org>") == expected
+
+
+def test_equal_in_url():
+    expected = "https://=.org"
+    assert message.extract_url("<https://=.org>") == expected
+
+
+def test_qmark_in_url():
+    expected = "https://?.org"
+    assert message.extract_url("<https://?.org>") == expected
+
+
+def test_and_in_url():
+    expected = "https://&.org"
+    assert message.extract_url("<https://&.org>") == expected
+
+
+def test_hyphen_in_url():
+    expected = "https://-.org"
+    assert message.extract_url("<https://-.org>") == expected
+
+
+def test_underscore_in_url():
+    expected = "https://_.org"
+    assert message.extract_url("<https://_.org>") == expected
 
 
 def test_detection_of_http():
@@ -16,13 +50,3 @@ def test_detection_of_http():
 
 def test_message_does_not_have_a_url():
     assert message.extract_url("foo <htts://example.org>") is None
-
-
-def test_url_does_not_start_with_smaller_than_symbol():
-    expected = "http://example.org"
-    assert message.extract_url("<<https://example.org>") == expected
-
-
-def test_url_does_not_end_with_larger_than_symbol():
-    expected = "http://example.org"
-    assert message.extract_url("<https://example.org>>") == expected
