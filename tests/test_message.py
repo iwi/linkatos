99dc2aafa0958897b25ef396e8484a5d867db490
iwi <iwi@tuta.io>
2016-12-10 18:04:06 +0000

    test: Change test urls to more realistic ones

--- tests/test_message.py
+++ tests/test_message.py
@@ -6,47 +6,57 @@ import linkatos.message as message
 
 def test_basic_url_detection_of_https():
     expected = "https://example.org"
-    assert message.extract_url("foo <https://example.org>") == expected
+    text = "foo <https://example.org>"
+    assert message.extract_url(text) == expected
 
 def test_hash_in_url():
-    expected = "https://#.org"
-    assert message.extract_url("<https://#.org>") == expected
+    expected = "https://foo.org#bar"
+    text = "<https://foo.org#bar>"
+    assert message.extract_url(text) == expected
 
 
 def test_plus_in_url():
-    expected = "https://+.org"
-    assert message.extract_url("<https://+.org>") == expected
+    expected = "http://foo.org/foo+bar"
+    text = "<http://foo.org/foo+bar>"
+    assert message.extract_url(text) == expected
 
 
 def test_equal_in_url():
-    expected = "https://=.org"
-    assert message.extract_url("<https://=.org>") == expected
+    expected = "https://wikipedia.org/title=foo"
+    text = "<https://wikipedia.org/title=foo>"
+    assert message.extract_url(text) == expected
 
 
 def test_qmark_in_url():
-    expected = "https://?.org"
-    assert message.extract_url("<https://?.org>") == expected
+    expected = "http://wik.org/index.php?title"
+    text = "<http://wik.org/index.php?title>"
+    assert message.extract_url(text) == expected
 
 
 def test_and_in_url():
-    expected = "https://&.org"
-    assert message.extract_url("<https://&.org>") == expected
+    expected = "https://foo.org/alpha&beta"
+    text = "<https://foo.org/alpha&beta>"
+    assert message.extract_url(text) == expected
 
 
 def test_hyphen_in_url():
-    expected = "https://-.org"
-    assert message.extract_url("<https://-.org>") == expected
+    expected = "https://this-is-a-test.org"
+    text = "<https://this-is-a-test.org>"
+    assert message.extract_url(text) == expected
 
 
 def test_underscore_in_url():
-    expected = "https://_.org"
-    assert message.extract_url("<https://_.org>") == expected
+    expected = "https://this_website.org"
+    text = "<https://this_website.org>"
+    assert message.extract_url(text) == expected
 
 
-def test_detection_of_http():
+def test_detection_of_http_in_the_middle():
     expected = "http://example.com"
-    assert message.extract_url("foo <http://example.com> bar") == expected
+    text = "foo <http://example.com> bar"
+    assert message.extract_url(text) == expected
 
 
 def test_message_does_not_have_a_url():
-    assert message.extract_url("foo <htts://example.org>") is None
+    text = "foo <htts://no_url_example.org>"
+    assert message.extract_url(text) is None
