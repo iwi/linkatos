f4f5a6ffa1fd60437b83bfc435a180ddf2433ea4
iwi <iwi@tuta.io>
2016-12-11 23:22:50 +0000

    test: Fix change of function names

--- tests/test_confirmation.py
+++ tests/test_confirmation.py
@@ -6,7 +6,7 @@ def test_no_confirmation_with_url():
     expecting_confirmation = False
     parsed_m = {'out': 'http://ex.org', 'channel': 'ch', 'type': 'url'}
 
-    assert confirmation.update_confirmation_if_url(parsed_m,
+    assert confirmation.update_if_url(parsed_m,
                                                    expecting_confirmation) is True
 
 
@@ -16,7 +16,7 @@ def test_confirmation_with_url():
     expecting_confirmation = True
     parsed_m = {'out': 'http://ex.org', 'channel': 'ch', 'type': 'url'}
 
-    assert confirmation.update_confirmation_if_url(parsed_m,
+    assert confirmation.update_if_url(parsed_m,
                                                    expecting_confirmation) is True
 
 
@@ -24,7 +24,7 @@ def test_confirmation_without_url():
     expecting_confirmation = True
     parsed_m = {'out': None, 'channel': 'ch', 'type': None}
 
-    assert confirmation.update_confirmation_if_url(parsed_m,
+    assert confirmation.update_if_url(parsed_m,
                                                 expecting_confirmation) is True
 
 
@@ -32,5 +32,5 @@ def test_no_confirmation_without_url():
     expecting_confirmation = False
     parsed_m = {'out': None, 'channel': 'ch', 'type': None}
 
-    assert confirmation.update_confirmation_if_url(parsed_m,
+    assert confirmation.update_if_url(parsed_m,
                                                expecting_confirmation) is False
