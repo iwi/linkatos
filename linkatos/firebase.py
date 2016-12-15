69738f10a6e22cbc391749ddff17e03462689728
iwi <iwi@tuta.io>
2016-12-15 20:55:28 +0000

    refactor: Extract functions out of the main firebase one.

--- linkatos/firebase.py
+++ linkatos/firebase.py
@@ -11,16 +11,41 @@ def initialise(api_key, project_name):
 
     return pyrebase.initialize_app(config)
 
+################################## user???????????????????
+def authenticate_user(user, password, firebase):
+    auth = firebase.auth()
+    user = auth.sign_in_with_email_and_password(user, password)
+    return user
 
-def store_url(is_yes, url, user, password, firebase):
+
+def get_db(firebase):
+    return firebase.database()
+
+
+def get_token(user):
+    return user['idToken']
+
+
+def store_url(url, db, token):
+    return db.push(to_data(url), token)
+
+
+def to_data(url):
+    return {"url": url}
+
+
+
+def xxxxxxxxxx(is_yes, url, user, password, firebase):
     # do nothing if it's unnecessary
     if not is_yes:
         return False
 
+    db = get_db(firebase)
+    user = authenticate_user(user, password, firebase)
+    token = get_token(user, password, firebase)
+
     # creates token every time maybe worth doing it once every 30m as they
     # expire every hour
-    auth = firebase.auth()
-    user = auth.sign_in_with_email_and_password(user, password)
 
     db = firebase.database()
     data = {
