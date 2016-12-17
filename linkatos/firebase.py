2007c7190f95a2656715e99af7ca632bbb98b313
iwi <iwi@tuta.io>
2016-12-17 11:36:49 +0000

    refactor: Change based on PR comments

--- linkatos/firebase.py
+++ linkatos/firebase.py
@@ -12,13 +12,9 @@ def initialise(api_key, project_name):
     return pyrebase.initialize_app(config)
 
 
-def authenticate_user(username, password, auth):
+def authenticate(username, password, auth):
     user = auth.sign_in_with_email_and_password(username, password)
     return user
-
-
-def get_db(firebase):
-    return firebase.database()
 
 
 def get_token(user):
@@ -29,19 +25,14 @@ def store_url(url, db, token):
     return db.child("users").push(to_data(url), token)
 
 
-def get_auth(firebase):
-    return firebase.auth()
-
-
 def to_data(url):
     return {"url": url}
 
 
 def connect_to_fb_and_store_url(url, username, password, firebase):
     # the function should only be called if we need to store the url
-    auth = get_auth(firebase)
-    user = authenticate_user(username, password, auth)
+    auth = firebase.auth()
+    user = authenticate(username, password, auth)
     token = get_token(user)
-    data = to_data(url)
-    db = get_db(firebase)
+    db = firebase.database()
     store_url(url, db, token)
