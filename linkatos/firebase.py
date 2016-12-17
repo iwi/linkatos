42415481c7087559cb4f2f829eb1745781b54cf7
iwi <iwi@tuta.io>
2016-12-17 15:31:30 +0000

    refactor: Change db set up

--- linkatos/firebase.py
+++ linkatos/firebase.py
@@ -18,8 +18,9 @@ def authenticate(credentials, auth):
     return user
 
 
-def get_token(username, password, firebase):
-    user = authenticate(username, password, firebase.auth())
+def get_token(credentials, firebase):
+    user = authenticate(credentials,
+                        firebase.auth())
     return user['idToken']
 
 
@@ -28,11 +29,15 @@ def to_data(url):
 
 
 def store_url(url, db, token):
-    return db.child("users").push(to_data(url), token)
+    return db.push(to_data(url), token)
+
+
+def urls_db(firebase):
+    return firebase.database().child('urls')
 
 
 def connect_and_store_url(url, credentials, firebase):
     # the function should only be called if we need to store the url
     token = get_token(credentials, firebase)
-    db = firebase.database()
+    db = urls_db(firebase)
     store_url(url, db, token)
