d18077c87b15796f1519e4debe035acec41f746b
iwi <iwi@tuta.io>
2016-12-21 08:51:16 +0000

    test: Add parse tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,60 +4,63 @@ import linkatos.parser as parser
 
 # test message parsing
 def test_ignore_linkatos_message():
-    input_example = [{
+    message = [{
         'user': 'bot_id',
         'channel': 'channel',
         'text': 'http://example.org'
     }]
 
-    output = {'out': None, 'channel': None, 'type': None}
-
-    assert parser.parse(input_example, 'bot_id') == output
-
-
-def test_is_of_url_type():
-    input_example = [{
-         'channel': 'channel',
-         'text': '<http://example.org>',
-         'user': 'user'
-    }]
-
-    output = {'out': 'http://example.org', 'channel': 'channel', 'type': 'url'}
-
-    assert parser.parse(input_example, 'bot_id') == output
+    parsed = {'message': None,
+              'channel': None,
+              'ts': None,
+              'item_ts': None,
+              'type': None,
+              'user': None,
+              'item_user': None}
 
 
-def test_is_of_ynanswer_type_yes():
-    input_example = [{
-         'channel': 'channel',
-         'text': 'yes',
-         'user': 'user'
-    }]
-
-    output = {'out': True, 'channel': 'channel', 'type': 'yn_answer'}
-
-    assert parser.parse(input_example, 'bot_id') == output
+    assert parser.parse(message, 'bot_id') == parsed
 
 
-def test_is_of_ynanswer_type_no():
-    input_example = [{
-         'channel': 'channel',
-         'text': 'No',
-         'user': 'user'
-    }]
+def test_capture_reaction():
+    message = {
+        'reaction': '+1',
+        'channel': 'example_channel',
+        'item': {
+            'channel': 'example_channel',
+            'ts': 1234.1234
+        },
+        'user': 'example_user',
+        'item_user': 'example_item_user'
+    }
 
-    output = {'out': False, 'channel': 'channel', 'type': 'yn_answer'}
+    reaction= {
+        'message': '+1',
+        'channel': 'example_channel',
+        'item_ts': 1234.1234,
+        'type': 'reaction',
+        'user': 'example_user',
+        'item_user': 'example_item_user'
+    }
 
-    assert parser.parse(input_example, 'bot_id') == output
+    assert parser.capture_reaction(message) == reaction
 
 
-def test_channelin_channelout():
-    input_example = [{
-         'channel': 'channel_in',
-         'text': 'yes',
-         'user': 'user'
-    }]
+def test_capture_url():
+    message = {
+        'channel': 'example_channel',
+        'ts': 1234.1234,
+        'user': 'example_user',
+    }
 
-    output = {'out': True, 'channel': 'channel_in', 'type': 'yn_answer'}
+    url = 'http://foo.bar'
 
-    assert parser.parse(input_example, 'bot_id') == output
+    parsed_url = {
+        'message': 'http://foo.bar',
+        'channel': 'example_channel',
+        'ts': 1234.1234,
+        'type': 'url',
+        'user': 'example_user',
+    }
+
+    assert parser.capture_url(message, url) == parsed_url
