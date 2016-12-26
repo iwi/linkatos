2556a72b7c60e5c5d50e00bca84b1df94aaa2678
iwi <iwi@tuta.io>
2016-12-26 20:12:33 +0000

    style: Change comments indentation

--- linkatos.py
+++ linkatos.py
@@ -9,33 +9,32 @@ import linkatos.activities as activities
 
 # Main
 if __name__ == '__main__':
-# starterbot environment variables
+    # starterbot environment variables
     BOT_ID = os.environ.get("BOT_ID")
     SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 
-# instantiate Slack clients
+    # instantiate Slack clients
     slack_client = SlackClient(SLACK_BOT_TOKEN)
 
-# firebase environment variables
+    # firebase environment variables
     FB_API_KEY = os.environ.get("FB_API_KEY")
     FB_USER = os.environ.get("FB_USER")
     FB_PASS = os.environ.get("FB_PASS")
     fb_credentials = {'username': FB_USER, 'password': FB_PASS}
 
-# initialise firebase
+    # initialise firebase
     project_name = 'coses-acbe6'
     firebase = fb.initialise(FB_API_KEY, project_name)
 
-
-# verify linkatos connection
+    # verify linkatos connection
     if slack_client.rtm_connect():
         parsed_url_message = {}
         expecting_url = True
         expecting_reaction = False
 
         while True:
-# note that url is returned to keep it over several cylcles in
-# whilst we wait for an answer
+            # note that url message is returned to keep it over several cylcles
+            # in whilst we wait for an answer
             (expecting_url, expecting_reaction, parsed_url_message) = \
                 activities.event_consumer(
                     expecting_url,
