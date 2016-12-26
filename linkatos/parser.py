c6fe280c290d801199a0b90c10cd2007b4989afc
iwi <iwi@tuta.io>
2016-12-26 00:33:39 +0000

    refactor: Delete legacy code

--- linkatos/parser.py
+++ linkatos/parser.py
@@ -2,18 +2,6 @@ import message as message
 import utils
 
 
-def capture_reaction(message):
-    parsed = {
-        'message': message['reaction'],
-        'channel': message['item']['channel'],
-        'item_ts': message['item']['ts'],
-        'type': 'reaction',
-        'user': message['user'],
-        'item_user': message['item_user']
-    }
-    return parsed
-
-
 def parse_url_message(event):
     url = message.extract_url(text)
 
@@ -36,7 +24,7 @@ def parse_url_message(event):
         'user': event['user']
     }
 
-    return (url_message)
+    return url_message
 
 
 def parse_reaction_added(event):
@@ -49,54 +37,3 @@ def parse_reaction_added(event):
         'to_user': event['item_user']
     }
     return parsed
-
-
-# def parse(input_message):
-#     """
-#         The Slack Real Time Messaging API is an events firehose.
-#         this parsing function returns None unless:
-#         1. someone posts a url starting with httpS?//
-#            in this case it returns the url and a out_type = 'url'
-#         2. someone adds a thumbsup or a thumbsdown
-#            in this case it returns the reaction and the type of outcome
-#     """
-#     print('input_message:', input_message)  # print the list of outputs to get them on screen
-
-#     # default outcome
-#     parsed = {
-#         'message': None,
-#         'channel': None,
-#         'ts': None,
-#         'item_ts': None,
-#         'type': None,
-#         'user': None,
-#         'item_user': None
-#     }
-
-#     # if the message list is empty return an empty object
-#     if is_empty(input_message):
-#         return (parsed)
-
-#     for message in input_message:
-#         print('message:', message)
-
-#         # capture a reaction if the message is a thumbsup/down reaction
-#         if utils.has_reaction_keys(message) and \
-#            message['type'] == 'reaction_added' and \
-#            (message['reaction'] == '+1' or message['reaction'] == '-1'):
-#             parsed = capture_reaction(message)
-#             return parsed
-
-#         # extract url from text if there is text and it has a url
-#         if utils.has_text_keys(message):
-#             text = message['text']
-#             url = message.extract_url(text)
-
-#             # if a url was found return the relevant data
-#             if url is not None:
-#                 parsed = capture_url(message, url)
-#                 return parsed  # url output
-
-#     return parsed
-
-
