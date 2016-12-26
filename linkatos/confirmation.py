012730b744e15c2a7712c5f30e95d42d47e64bfe
iwi <iwi@tuta.io>
2016-12-26 20:30:09 +0000

    feat: Delete unused confirmation.py

--- linkatos/confirmation.py
+++
@@ -1,9 +0,0 @@
-import linkatos.printer as printer
-from .utils import is_fresh_url
-
-
-def update_if_url(parsed_message, expecting_confirmation):
-    if is_fresh_url(expecting_confirmation, parsed_message['type']):
-        expecting_confirmation = True
-
-    return (expecting_confirmation)
