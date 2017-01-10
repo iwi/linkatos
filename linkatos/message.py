a8c8664579cff095ce40b6540218d9933f8b04cf
iwi <iwi@tuta.io>
2017-01-10 20:40:12 +0000

    refactor: Delete compilation of regex

--- linkatos/message.py
+++ linkatos/message.py
@@ -1,14 +1,10 @@
 import re
 
-url_re = re.compile("(?:\s|^)<(https?://[\w./?+&+%$!#=\-_]+)>(?:\s|$)")
-purge_re = re.compile("(purge) (\d+)")
-list_re = re.compile("list")
-
 def extract_url(message):
     """
     Returns the first url in a message. If there aren't any returns None
     """
-    answer = url_re.search(message)
+    answer = re.search("(?:\s|^)<(https?://[\w./?+&+%$!#=\-_]+)>(?:\s|$)", message)
 
     if answer is not None:
         answer = answer.group(1).strip()
@@ -18,20 +14,19 @@ def extract_url(message):
 
 def to_bot(message, bot_id):
     bot_re = "^<@" + bot_id + '>'
-    to_bot_re = re.compile(bot_re)
-    bot_found = to_bot_re.search(message)
+    bot_found = re.search(bot_re, message)
 
     return bot_found is not None
 
 
 def is_list_request(message):
-    list_found = list_re.search(message)
+    list_found = re.search("list", message)
 
     return list_found is not None
 
 
 def purge_request(message):
-    index_found = purge_re.search(message)
+    index_found = re.search("(purge) (\d+)", message)
 
     if index_found is None:
         return None
