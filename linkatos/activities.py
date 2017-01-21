cd3cd7f89de46f4f0164491780504346b3094b68
iwi <iwi@tuta.io>
2017-01-21 10:24:04 +0000

    refactor: Add message_consumer and reaction_added_consumer

--- linkatos/activities.py
+++ linkatos/activities.py
@@ -42,20 +42,63 @@ def is_unfurled(event):
     return 'previous_message' in event
 
 
+def message_consumer(event, cache, slack_client, bot_id, fb_credentials,
+                     firebase):
+    """
+    Consumes an event of type 'message' that contains a user.
+    It looks inside the 'text' field for:
+        + a url
+        + @linkatos list
+        + @linkatos purge <i>
+       where <i> is the ith element of the cache
+    """
+    url = parser.parse_url_message(event)
+
+    if is_new(url, bot_id):
+        return cch.add(url, cache, slack_client)
+
+    if message.to_bot(event['text'], bot_id):
+        list_request = parser.parse_list_request(event)
+        purge_request = parser.parse_purge_request(event)
+
+        if list_request is not None and list_request['type'] == 'list_request':
+            cch.display(cache,
+                        list_request['channel'],
+                        slack_client)
+            return cache
+
+        if purge_request is not None and \
+           purge_request['type'] == 'purge_request' and \
+           not cch.is_empty(cache):
+            cch.extract_url_by_index(cache,
+                                     purge_request['index'] - 1)
+            cch.display(cache,
+                        purge_request['channel'],
+                        slack_client)
+            return cache
+
+
+def reaction_added_consumer(event, cache, fb_credentials, firebase):
+    """
+    Consumes an event of type 'reaction_added' that respond to a cached url
+    with either a :+1: or a :-1:
+    """
+    reaction = parser.parse_reaction_added(event)
+
+    if react.is_known(reaction['reaction']):
+        selected_url = cch.extract_url_by_id(cache,
+                                             reaction['to_id'])
+        react.handle(reaction['reaction'], selected_url['url'],
+                     fb_credentials, firebase)
+        return cache
+
 def event_consumer(cache, slack_client, bot_id, fb_credentials, firebase):
     """
     Consumes batches of slack events and runs the possible actions.
 
     Reads new batches of Slack events.
-    Selects those events that need to be actioned.
-    Actionable events are:
-        - of 'type' message and contain a url
-        - are of 'type' reaction_added and respond to a cached url with either
-        a :+1: or a :-1:
-        - of 'type' message and are either:
-            + @linkatos list
-            + @linkatos purge <i>
-            where <i> is the ith element of the cache
+    Selects those events that need to be actioned. Currently they must be of
+    type 'message' or of type 'reaction_added'
     """
     # Read slack events
     events = slack_client.rtm_read()
@@ -68,39 +111,10 @@ def event_consumer(cache, slack_client, bot_id, fb_credentials, firebase):
             return cache
 
         if event['type'] == 'message' and 'username' not in event:
-            url = parser.parse_url_message(event)
-
-            if is_new(url, bot_id):
-                return cch.add(url, cache, slack_client)
-
-            if message.to_bot(event['text'], bot_id):
-                list_request = parser.parse_list_request(event)
-                purge_request = parser.parse_purge_request(event)
-
-                if list_request is not None and list_request['type'] == 'list_request':
-                    cch.display(cache,
-                                list_request['channel'],
-                                slack_client)
-                    return cache
-
-                if purge_request is not None and \
-                   purge_request['type'] == 'purge_request' and \
-                   not cch.is_empty(cache):
-                    cch.extract_url_by_index(cache,
-                                             purge_request['index'] - 1)
-                    cch.display(cache,
-                                purge_request['channel'],
-                                slack_client)
-                    return cache
+            return message_consumer(event, cache, slack_client, bot_id,
+                                    fb_credentials, firebase)
 
         if event['type'] == 'reaction_added' and len(cache) > 0:
-            reaction = parser.parse_reaction_added(event)
-
-            if react.is_known(reaction['reaction']):
-                selected_url = cch.extract_url_by_id(cache,
-                                                     reaction['to_id'])
-                react.handle(reaction['reaction'], selected_url['url'],
-                             fb_credentials, firebase)
-                return cache
-
+            return reaction_added_consumer(event, cache, fb_credentials,
+                                           firebase)
     return cache
